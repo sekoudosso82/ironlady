ec7f37f74e3be4e608d2d6ff28b2eb3afa6008e2
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-21 20:21:08 -0400

    ironlady front end

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux' 
+import reducer from './reducer'
+import thunk from 'redux-thunk'
+
+
+let store = createStore(reducer, applyMiddleware(thunk))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <Provider store={store}>
+      <Router>
+        <Route path="/" component={App} />
+      </Router>
+  </Provider>
+, document.getElementById('root'));
