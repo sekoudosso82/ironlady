daf9f67ef8b3b3dbd15397eadaefb3c663835965
sekoudosso82 <sekou.dosso82@gmail.com>
2020-08-22 13:30:04 -0400

    Updates

---
+++ precache-manifest.839a911035dea210a09699654f77c2a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a132729e99eb93397628ae5f457a28",
+    "url": "/ironlady/index.html"
+  },
+  {
+    "revision": "5ef91100465fd39ea3d2",
+    "url": "/ironlady/static/css/main.812dd202.chunk.css"
+  },
+  {
+    "revision": "0ae276910eab21f16a82",
+    "url": "/ironlady/static/js/2.f234d671.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ironlady/static/js/2.f234d671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef91100465fd39ea3d2",
+    "url": "/ironlady/static/js/main.b3e75001.chunk.js"
+  },
+  {
+    "revision": "47c4ba0af0efc75f364c",
+    "url": "/ironlady/static/js/runtime-main.08f3a600.js"
+  },
+  {
+    "revision": "7479a9f91cc9924d6ff3387f3cb7a020",
+    "url": "/ironlady/static/media/bp.7479a9f9.jpg"
+  },
+  {
+    "revision": "cd8dd53e577a484a9d70ad992f6c7684",
+    "url": "/ironlady/static/media/bp3.cd8dd53e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ironlady/static/media/logo.5d5d9eef.svg"
+  }
+]);
